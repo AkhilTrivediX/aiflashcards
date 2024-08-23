7bc87368ea28dd109ee77af8a223529909683f0a
DevilsKingX <118957648+AkhilTrivediX@users.noreply.github.com>
2024-08-23 17:50:53 +0530

    Added Demo Video Link

--- components/WaitlistCall.tsx
+++ components/WaitlistCall.tsx
@@ -1,15 +1,23 @@
 import { Tomorrow } from "next/font/google"
+import { FaLink } from "react-icons/fa"
+import { IoSparkles } from "react-icons/io5"
 
 
 const tomorrow = Tomorrow({ weight: ['300','600'], subsets: ['latin'] })
 
 export default function WaitlistCall(){
     return (<div className="flex flex-col w-screen h-screen relative overflow-hidden items-center justify-between">
-        <a href="/waitlist" >
-        <div className={"mt-4 bg-foreground text-background font-bold w-[max-content] px-4 py-2 sharpCorner-tl relative "+(tomorrow.className)}>
-                <div className=" testScoreBG tenCardsBG"></div>
-                Join Waitlist
-        </div></a>
+        <div className="flex gap-4">
+            <a href="/waitlist" >
+            <div className={"mt-4 bg-foreground text-background font-bold w-[max-content] px-4 py-2 sharpCorner-tl relative flex items-center gap-2 "+(tomorrow.className)}>
+                    <div className=" testScoreBG tenCardsBG"></div>
+                    Join Waitlist <IoSparkles/>
+            </div></a>
+            <a href="https://youtu.be/xz9mEDCByqI" target="_blank">
+            <div className={"mt-4 bg-foreground text-background font-bold w-[max-content] px-4 py-2 sharpCorner-tl relative flex items-center gap-2 "+(tomorrow.className)}>
+                    Demo Video <FaLink/>
+            </div></a>
+        </div>
         <div className="w-full h-full bg-background ">
             <iframe src={"https://player.vimeo.com/video/1001945210?background=1"} className={"w-full h-full object-cover "}></iframe>
         </div>
